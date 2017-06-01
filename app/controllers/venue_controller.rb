6d56203da28f46b26720bdee7b714a4d4661cde9
Andrew Williams <andrewleighwilliams@gmail.com>
2017-05-31 21:37:11 -0400

    Generate rails controller files for venue and implement venue_controller functions

---
+++ app/controllers/venue_controller.rb
@@ -0,0 +1,50 @@
+class VenueController < ApplicationController
+	def list
+		@venues = Venue.all
+   end
+   
+   def show
+   	@venue = Venue.find(params[:id])
+   end
+   
+   def new
+   	@venue = Venue.new
+   end
+   
+   def create
+   	@venue = Venue.new(venue_params)
+
+   	if @venue.save
+   		redirect_to :action => 'list'
+   	else
+   		render :action => 'new'
+   	end
+   end
+
+   def venue_params
+   	params.require(:venues).permit(:name, :address, :city, :state)
+   end
+   
+   def edit
+   	@book = Book.find(params[:id])
+   end
+   
+   def update
+   	@venue = Venue.find(params[:id])
+
+   	if @venue.update_attributes(venue_param)
+   		redirect_to :action => 'show', :id => @venue
+   	else
+   		render :action => 'edit'
+   	end
+   end
+
+   def venue_param
+   	params.require(:venue).permit(:name, :address, :city, :state)
+   end
+   
+   def delete
+    Venue.find(params[:id]).destroy
+    redirect_to :action => 'list'
+   end
+end
