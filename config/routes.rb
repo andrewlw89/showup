a3f773190544ad91e4684925b88ae905fd61b63f
Andrew Williams <andrewleighwilliams@gmail.com>
2017-05-31 22:02:44 -0400

    Stage routes for commit. Implement list and new views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'venue/list'
+  get 'venue/new'
+  post 'venue/create'
+  patch 'venue/update'
+  get 'venue/list'
+  get 'venue/show'
+  get 'venue/edit'
+  get 'venue/delete'
+  get 'venue/update'
 end
